0714d70ea810ed96f18d9b327785167123b0bbc6
freddy velarde <freddyvelarde59@gmail.com>
2022-09-11 18:24:35 -0400

    adding theme colors

---
+++ client/src/global-styles/colors.jsx
@@ -0,0 +1,19 @@
+export const darkTheme = {
+  // primary: "#1D2328",
+  // secondary: "#303439",
+  primary: "#20233F",
+  secondary: "#262949",
+  success: "#F44B9A",
+  button: "#2F81FF",
+  titles: "#ffffff",
+  gray: "#76797F",
+};
+
+export const lightTheme = {
+  primary: "#ffffff",
+  secondary: "#F6F8FA",
+  success: "#4AC9C9",
+  button: "#F6F8FA",
+  titles: "#000000",
+  gray: "#515B64",
+};
