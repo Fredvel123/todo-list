7b0a342b748481ba254d638e81df00970260cd8c
freddy velarde <freddyvelarde59@gmail.com>
2022-09-12 12:04:31 -0400

    theme global state added with react redux

---
+++ client/src/redux/slices/theme.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const themeSlice = createSlice({
+  name: "theme",
+  initialState: { value: true },
+  reducers: {
+    setTheme: (state, actions) => {
+      state.value = actions.payload;
+    },
+  },
+});
+
+export const { setTheme } = themeSlice.actions;
+export default themeSlice.reducer;
